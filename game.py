3d0977b60374cd097ea2324f659297c801c31ae4
Matej Kovaľ <kovalmatej@gmail.com>
2019-11-28 23:31:59 +0100

    Vytvorenie prázdnej šachovnice

--- game.py
+++ game.py
@@ -1,3 +1,11 @@
 n = int(input("Zadaj velkost šachovnice"))
 
-a = [[0] * n for i in range(n)]
+def generateBoard(n):
+    gameBoard = [[' '] * n for i in range(n)] # Vytvorí prázdne miesto pre všetky prvky matice
+    return gameBoard
+
+def drawBoard(gameBoard):
+    for row in gameBoard:
+        print(' '.join([element for element in row])) # Vypíše všetky elementy v riadku a spojí ich medzerou
+
+drawBoard(generateBoard(n))
