83bb49bda39e6f59571f48d96a0b5e614db4f13c
Matej Kovaľ <kovalmatej@gmail.com>
2019-11-29 15:09:59 +0100

    Pridané pozície

--- game.py
+++ game.py
@@ -3,7 +3,7 @@ import random
 #MAIN GAME FUNCTIONS
 
 def create_game(board_size):
-    return generate_board(board_size)[0]
+    return generate_board(board_size)
 
 def throw_dice():
     return random.randint(1, 6)
@@ -12,9 +12,9 @@ def game_status(counter):
     throwed_number = throw_dice()
 
     if counter % 2 == 0:
-        print("Na rade je hrac A a hodil cislo!", throwed_number)
+        print("Na rade je hrac A a hodil cislo: ", throwed_number)
     else:
-        print("Na rade je hrac B!", throwed_number)
+        print("Na rade je hrac B a hodil cislo: ", throwed_number)
     return throwed_number
 
 
@@ -30,7 +30,8 @@ def start_game(game_board):
         counter += 1
 
 
-
+        if counter == 5:
+            break;
 
         draw_board(game_board)
 
@@ -39,7 +40,6 @@ def start_game(game_board):
 
 def generate_board(n):
     game_board = [[' '] * n for i in range(n)]  # Vytvorí prázdne miesto pre všetky prvky matice
-    positions =
 
     for i in range(n):
         for j in range(n):
@@ -51,12 +51,19 @@ def generate_board(n):
                 game_board[i][j] = '*'
                 game_board[j][i] = '*'
 
+
+
+
+
+
             if (j == (n - 1) / 2) & (i != n - 1):  # Vykresli domčeky v strede šachovnice
                 game_board[i][j] = 'D'
                 game_board[j][i] = 'D'
 
+               # positions.append([i, j])
+
     game_board[int((n - 1) / 2)][int((n - 1) / 2)] = 'X'
-    return [game_board, positions]
+    return game_board
 
 
 def draw_board(game_board):
@@ -64,7 +71,39 @@ def draw_board(game_board):
         print(' '.join([element for element in row]))  # Vypíše všetky elementy v riadku a spojí ich medzerou
 
 def get_positions(game_board):
-    32
+    positions = []
+
+    for i in range(int((board_size - 1) / 2)):
+        for j in range(int((board_size - 1) / 2) + 1, board_size):
+            if ((game_board[i][j] != ' ') & (game_board[i][j] != 'D') & (game_board[i][j] != 'X')):
+                positions.append([i, j])
+
+    positions.append([int((board_size - 1) / 2), board_size - 1])
+
+    for i in range(int((board_size - 1) / 2) + 1, board_size - 1):
+        for j in range(board_size - 1, int((board_size - 1) / 2), -1):
+
+            if ((game_board[i][j] != ' ') & (game_board[i][j] != 'D') & (game_board[i][j] != 'X')):
+                positions.append([i, j])
+
+    positions.append([board_size - 1, int((board_size - 1) / 2) + 1])
+
+    for i in range(board_size - 1,int((board_size - 1) / 2), -1):
+        for j in range(int((board_size - 1) / 2), 0, -1):
+            if ((game_board[i][j] != ' ') & (game_board[i][j] != 'D') & (game_board[i][j] != 'X')):
+                positions.append([i, j])
+
+    positions.append([int((board_size - 1) / 2) + 1, 0])
+
+    for i in range(int((board_size - 1) / 2), 0, -1):
+        for j in range(0, int((board_size - 1) / 2)):
+            if ((game_board[i][j] != ' ') & (game_board[i][j] != 'D') & (game_board[i][j] != 'X')):
+                positions.append([i, j])
+
+    positions.append([0, int((board_size - 1) / 2) - 1])
+
+    return positions
+
 
 #PLAYERS FUNCTIONS
 
@@ -86,3 +125,5 @@ board_size = int(input("Zadaj velkost šachovnice"))
 current_game = create_game(board_size)
 
 start_game(current_game)
+
+print(get_positions(current_game))
